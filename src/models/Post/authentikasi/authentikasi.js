6f55642299a25cdca9d26d36859f59894b86f0f6
kevinmaulanan <kevinmaulana2703@gmail.com>
2020-04-01 16:46:35 +0700

    login user

--- src/models/Post/authentikasi/authentikasi.js
+++ src/models/Post/authentikasi/authentikasi.js
@@ -7,15 +7,16 @@ const uuid = require('uuid').v1
 module.exports = {
     loginAuthentikasi: (username, password) => {
         return new Promise((resolve, reject) => {
-            db.query(`SELECT COUNT(*) as total FROM users_privat where username='${username}'`, (error, result) => {
+            db.query(`SELECT COUNT(*) as total FROM user_privates where username='${username}'`, (error, result) => {
                 const { total } = result[0]
                 if (total !== 1) {
                     reject(new Error('Username tidak ada'))
                 } else {
-                    db.query(`SELECT *FROM users_privat where username='${username}'`, (error, result) => {
-                        if (error) {
+                    db.query(`SELECT *FROM user_privates where username='${username}'`, (error, result) => {
+                        const isVerify = result[0].is_verify
+                        if (isVerify === 0) {
                             console.log(error)
-                            reject(new Error('Ada yang salah di sistem login'))
+                            reject(new Error('Username tersebut belum di verifikasi, silahkan cek email terlebih dahulu'))
                         } else {
                             const passwordData = result[0].password
                             const comparePassword = bcryptjs.compareSync(password, passwordData)
@@ -23,7 +24,6 @@ module.exports = {
                                 reject(new Error('Password Salah'))
                             } else {
                                 const data = result[0]
-
                                 resolve(data)
 
                             }
@@ -40,20 +40,20 @@ module.exports = {
             if (username === "" || password === "" || email === "") {
                 reject(new Error('Field Tidak Boleh Kosong'))
             } else {
-                db.query(`SELECT COUNT(*) as total FROM users_privat where username='${username}'`, (error, result) => {
+                db.query(`SELECT COUNT(*) as total FROM user_privates where username='${username}'`, (error, result) => {
                     const { total } = result[0]
                     if (total !== 0) {
                         reject(new Error('Username sudah digunakan'))
                     } else {
-                        db.query(`INSERT INTO users_detail (name_user) VALUES('${username}')`, (error, result) => {
+                        db.query(`INSERT INTO user_details (user_fullname) VALUES('${username}')`, (error, result) => {
                             if (error) {
                                 reject(new Error('Salah pada saat membuat query di users_detail'))
                             } else {
-                                db.query(`select max(id) as id from users_detail`, (error, result) => {
+                                db.query(`select max(id) as id from user_details`, (error, result) => {
                                     const maxId = result[0].id
                                     const encryptedPassword = bcryptjs.hashSync(password)
                                     const verify = uuid()
-                                    db.query(`INSERT INTO users_privat (username,password,email,verify, id_user_details) VALUES('${username}','${encryptedPassword}','${email}', '${verify}', ${maxId})`, (error, result) => {
+                                    db.query(`INSERT INTO user_privates (username,password,email,verify, id_user_detail) VALUES('${username}','${encryptedPassword}','${email}', '${verify}', ${maxId})`, (error, result) => {
                                         if (error) {
                                             console.log('sini')
                                             reject(new Error('Error database sistem'))
