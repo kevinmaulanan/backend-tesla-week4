83001211665bbf28c1a76fbcc5b30e98dda0de5b
agusrichard <agus.richard21@gmail.com>
2020-04-03 00:40:28 +0700

    Create pagination on get books by author

--- src/controller/Get/Books/books.js
+++ src/controller/Get/Books/books.js
@@ -31,7 +31,6 @@ const getBookByGenreId = async (req, res) => {
     try {
         const id = req.params.id
         const { data, total } = await books.getBooksByGenreId(id, req)
-        console.log('total', total)
         const pagination = paginate(req, `books/genre/${id}`, total)
         if (data) {
             res.status(200).send({
@@ -57,12 +56,14 @@ const getBookByGenreId = async (req, res) => {
 const getBooksByAuthorId = async (req, res) => {
     try {
         const authorId = req.params.id
-        const booksData = await books.getBooksByAuthorId(authorId)
-        if (booksData) {
+        const { data, total } = await books.getBooksByAuthorId(authorId, req)
+        const pagination = paginate(req, `books/author/${authorId}`, total)
+        if (data) {
             res.status(200).send({
                 success: true,
                 message: 'Success to get books by author id',
-                data: booksData
+                data,
+                pagination
             })
         } else {
             res.status(404).send({
--- src/models/Get/books/books.js
+++ src/models/Get/books/books.js
@@ -88,20 +88,23 @@ module.exports = {
     },
 
 
-    getBooksByAuthorId: (authorId) => {
+    getBooksByAuthorId: (authorId, req) => {
+        const { conditions, paginate } = paginationParams(req)
         return new Promise((resolve, reject) => {
-            db.query(`SELECT COUNT(*) as total from books where id_author=${authorId}`, (error, result) => {
-                const { total } = result[0]
+            db.query(`SELECT books.id, books.book_name, books.description, global_book_ratings.total_reviewers, global_book_ratings.avg_rating, authors.author_name FROM books JOIN authors ON books.id_author=authors.id JOIN global_book_ratings ON books.id_global_rating=global_book_ratings.id ${conditions} AND authors.id=${authorId} && books.is_deleted=0 `, (error, result) => {
+                if (error) reject(error)
+                const total = result.length
                 if (total < 1) {
                     reject(new Error(`Author with id ${authorId} is not found`))
                 }
                 else {
-                    db.query(`SELECT books.id, books.book_name, books.description, global_book_ratings.total_reviewers, global_book_ratings.avg_rating, genres.genre_name, authors.author_name FROM books JOIN authors ON books.id_author=authors.id JOIN bridge_books_genres ON books.id= bridge_books_genres.id_book JOIN genres ON bridge_books_genres.id_genre=genres.id JOIN lists ON books.id=lists.id JOIN global_book_ratings ON books.id_global_rating=global_book_ratings.id WHERE authors.id=${authorId} && books.is_deleted=0 `, (error, result) => {
+                    db.query(`SELECT books.id, books.book_name, books.description, global_book_ratings.total_reviewers, global_book_ratings.avg_rating, authors.author_name FROM books JOIN authors ON books.id_author=authors.id JOIN global_book_ratings ON books.id_global_rating=global_book_ratings.id ${conditions} AND authors.id=${authorId} && books.is_deleted=0 ${paginate}`, (error, result) => {
                         if (error) {
+                            console.log(error)
                             reject(new Error(`Server error: Failed to get books by author`))
                         } else {
                             const data = result
-                            resolve(data)
+                            resolve({ data, total })
                         }
                     })
                 }
