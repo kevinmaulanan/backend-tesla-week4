67fba742e2b6d92b32c7b1729e4c805751926d04
agusrichard <agus.richard21@gmail.com>
2020-04-03 16:27:01 +0700

    Solve conflicting folder name

--- src/controller/post/auth/auth.js
+++ src/controller/post/auth/auth.js
@@ -1,4 +1,4 @@
-const authModel = require('../../../models/Post/auth/auth')
+const authModel = require('../../../models/post/auth/auth')
 const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
