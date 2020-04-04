413584ca89ed8d7b55960d6ad08a398ab9f07931
kevinmaulanan <kevinmaulana2703@gmail.com>
2020-04-04 16:26:11 +0700

    Renaming Middleware

--- src/routes/list/list.js
+++ src/routes/list/list.js
@@ -4,7 +4,7 @@ const { addList } = require('../../controller/post/list/list')
 const { deleteList } = require('../../controller/delete/list/list')
 const { getListByIdUserLogin } = require('../../controller/get/list/list')
 
-const { checkAuthToken } = require('../../Middleware/auth/auth')
+const { checkAuthToken } = require('../../middleware/auth/auth')
 
 list.delete('/', checkAuthToken, deleteList)
 list.get('/', checkAuthToken, getListByIdUserLogin)
