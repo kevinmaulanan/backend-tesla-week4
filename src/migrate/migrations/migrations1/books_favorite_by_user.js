08fccd7f97c43da12ca79ca3257398e90a7c0634
agusrichard <agus.richard21@gmail.com>
2020-04-04 17:11:18 +0700

    Change on migration

---
+++ src/migrate/migrations/migrations1/books_favorite_by_user.js
@@ -0,0 +1,15 @@
+const { db } = require('../../index')
+
+db.query(
+    `CREATE TABLE books_favorite_by_user (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      id_user INT,
+      id_book INT,
+      FOREIGN KEY (id_user) REFERENCES user_details(id) ON DELETE CASCADE ON UPDATE CASCADE,
+      FOREIGN KEY (id_book) REFERENCES books(id) ON DELETE CASCADE ON UPDATE CASCADE
+    );`,
+    (error, results, fields) => {
+      if (error) throw error
+      else console.log('books_favorite_by_user table is created successfully')
+    }
+  )
